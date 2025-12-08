6a576abdee5462a810efa8e598b45ba5b3df7102
AbhishekKumar Demo <abhishekkumar1106@gmail.in>
2025-12-08 19:18:40 +0530

    backend updated

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,9 @@ import validator from "validator";
 import bcrypt from 'bcrypt';
 import userModel from '../models/userModel.js';
 import jwt from 'jsonwebtoken';
+import { v2 as cloudinary } from 'cloudinary';
+import doctorModel from '../models/doctorModel.js';
+import appointmentModel from "../models/appointmentModel.js";
 
 //API to register user
 const registerUser = async (req, res) => {
@@ -42,6 +45,8 @@ const registerUser = async (req, res) => {
   }
 };
 
+
+
 //API for user login
 const loginUser = async(req, res) => {
   try {
@@ -72,5 +77,98 @@ const loginUser = async(req, res) => {
   }
 }
 
+//API for get user's profile
 
-export { registerUser, loginUser}
+const getProfile = async(req, res) => {
+  try {
+
+    const { userId } = req.body;
+    const userData = await userModel.find(userId).select('-password')
+
+    res.json({success: true, userData})
+    
+  } catch (error) {
+     console.log(error)
+        res.json({success: false, message: error.message})
+    
+  }
+};
+
+//API to update user's profile
+const updateProfile = async (req, res) => {
+  try {
+    const { userId, name, phone, adress, dob, gender } = req.body;
+    const  imageFile  = req.file
+
+  if (!name || !phone || !dob || !gender) {
+    return res.json({success: false, message: "Data missings"})
+    
+  }
+
+  await userModel.findByIdAndUpdate(userId, {name, phone, adress: JSON.parse(adress), dob, gender})
+
+  if (imageFile) {
+    const imageUpload = await cloudinary.uploader.upload(imageFile.path, {resource_type: 'image'})
+    const imageUrl = imageUpload.secure_url
+
+    await userModel.findByIdAndUpdate(userId, {image: imageUrl})
+  }
+
+  res.json({ success: true, message: 'Profile Updated'})
+    
+  } catch (error) {
+     console.log(error)
+        res.json({success: false, message: error.message})
+  }
+}
+
+//API for book appointment
+const bookAppointment = async(req, res) => {
+  try {
+    const { userId, docId, slotDate, slotTime } = req.body
+
+    const docData = await doctorModel.findById(docId).select('-password')
+
+    if (!docData.available) {
+      return res.json({ success: false, message:"doctor not available"})
+    }
+
+    let slots_booked = docData.slots_booked
+
+    if (slots_booked[slotDate]) {
+      if (slots_booked[slotDate].includes(slotTime)) {
+        return res.json({ success: false, message: "slot not available"})
+      } else {
+        slots_booked[slotDate].push(slotTime)
+      }
+    } else {
+      slots_booked[slotDate] = []
+      slots_booked[slotDate].push(slotTime)
+    }
+
+    const userData = await userModel.findById(userId).select('-password')
+
+    delete docData.slots_booked
+
+    const appointmentData = {
+      userId, docId, userData,docData,amount: docData.fees, slotTime, slotDate,date: Date.now()
+
+    }
+
+    const newAppointment = new appointmentModel(appointmentData)
+
+    await newAppointment.save()
+
+    // save new slots data in docData
+    await doctorModel.findByIdAndUpdate(docId, {slots_booked})
+
+    res.json({success: true, message: "Appointment Booked"})
+    
+  } catch (error) {
+    console.log(error)
+        res.json({success: false, message: error.message})
+  }
+}
+
+
+export { registerUser, loginUser, getProfile, updateProfile, bookAppointment}
---
+++ backend/middlewares/authUser.js
@@ -0,0 +1,29 @@
+import jwt from 'jsonwebtoken';
+
+
+//User authentication middleware
+const authUser = async(req, res, next) => {
+    try {
+
+        const {token} = req.headers
+        if (!token) {
+            return res.json({success: false, message: "not authorized Login Again"})
+        }
+        const token_decode = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.body.userId = token_decode.userId
+        next()
+
+       
+        next()
+
+        
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message})
+        
+    }
+
+}
+
+export default authUser
